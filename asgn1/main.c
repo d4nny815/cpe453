ff05fb1b52423c1478b0cfbbf1c20c670f198a8c
danny <d4nny815@gmail.com>
2024-09-28 09:49:56 -0700

    trying

--- asgn1/main.c
+++ asgn1/main.c
@@ -1,10 +1,17 @@
 #include "stdio.h"
-#include "libmalloc.h"
+#include "malloc.h"
+
+#define PRINT_BUF_SIZE (1500)
+
 
 int main(int argc, char** argv) {
-  printf("size of heapinfo %ld, size of heapchunk %ld\n", sizeof(struct HeapInfo_t), sizeof(struct Heapchunk_t));
+  // printf("size of heapinfo %ld, size of heapchunk %ld\n", sizeof(struct HeapInfo_t), sizeof(struct Heapchunk_t));
 
-  init_heap();
+  char print_buf[PRINT_BUF_SIZE];
+  snprintf(print_buf, PRINT_BUF_SIZE, "hello world\n");
+  puts(print_buf);
+
+  // init_heap();
 
   return 0;
 }
