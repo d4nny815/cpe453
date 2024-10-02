e950bca18e1c8a2348ba1d271f5038d0e90c2e61
danny <d4nny815@gmail.com>
2024-10-02 13:11:15 -0700

    main back

--- asgn1/main.c
+++ asgn1/main.c
@@ -1,30 +1,54 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <lib.h>
+#include "stdio.h"
+#include "malloc.h"
 
-/* allocate three buffers, then shrink the middle one.  It is not expected
- * to move */
+#define PRINT_BUF_SIZE (1500)
+static char buf[PRINT_BUF_SIZE];
 
-int main(int argc, char *argv[]){
-  unsigned char *one, *two, *three,*new;
+// int main(int argc, char** argv) {
+int main(void) {
+    print_heap();
+    char *p1, *p2, *p3, *p4;
+    int i;
+    for (i = 0; i < 8192; i++) {
+        size_t size = i;
+        p1 = malloc(size);
+        if ((long)p1 & (long)0xf) {
+            return 1;
+        }
+        snprintf(buf, PRINT_BUF_SIZE, "%p", p1);
+        puts(buf);
+    }
+    
+    print_heap();
+    /*
+    //p1 = (char*) malloc(PRINT_BUF_SIZE);
+    p1 = malloc(1024);
+    p2 = (char*) malloc(2 * PRINT_BUF_SIZE);
+    p3 = (char*) malloc(PRINT_BUF_SIZE / 2);
+    p4 = (char*) malloc(PRINT_BUF_SIZE / 10);
+    snprintf(buf, PRINT_BUF_SIZE, "p1: %p, p2: %p, p3: %p, p4:%p", 
+            p1, p2, p3, p4);
+    puts(buf);
+    print_heap();
 
-  one = getbuff(2048);          /* allocate and fill buffers */
-  two = getbuff(2048);
-  three = getbuff(2048);
+    free(p1);
+    free(p3);
+    fprintf(stderr, "free p1 and p3\n");
+    print_heap();
 
-  new = realloc(two,1024);
-  if ( new != two ) {
-    printf("Realloc unexpectedly moved the buffer old %p != new %p.\n",two,new);
-  }
+    p1 = malloc(PRINT_BUF_SIZE / 2);
+    p3 = malloc(PRINT_BUF_SIZE * 2);
+    fprintf(stderr, "p1: %p, p2: %p, p3: %p, p4:%p\n", p1, p2, p3, p4);
+    print_heap();
 
-  if ( check(new,1024,0) )
-    printf("Some values were not properly copied.\n");
-  if ( check(one,2048,0) )
-    printf("Values in buffer one were disturbed\n");
-  if ( check(three,2048,0) )
-    printf("Values in buffer three were disturbed\n");
+    free(p2);
+    fprintf(stderr, "free p2\n");
+    print_heap();
 
-  exit(0);
+    free(p4);
+    fprintf(stderr, "free p4\n");
+    print_heap();
+    */
+  return 0;
 }
+
